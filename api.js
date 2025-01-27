fe0f87b70796f36a2222fae019558fb7c6cf78bd
mk-pmb <mk-pmb@users.noreply.github.com>
2025-01-27 05:15:01 +0100

    v0.1.0: Auto-ungzip works for node.js.

---
+++ api.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+const EX = {
+  LibPsd() { throw new Error('Stub!'); },
+  async gunzip(data) { throw new Error('Stub!', data); },
+  async fromFile(path) { throw new Error('Stub!', path); },
+
+
+  async autoDecompress(bytes) {
+    const peek = String.fromCharCode(...bytes.slice(0, 2));
+    if (peek.startsWith('\x1F\x8B')) { return this.ungzip(bytes); }
+    return bytes;
+  },
+
+
+  async fromUint8Array(fileBytes) {
+    const psdBytes = await this.autoDecompress(fileBytes);
+    const psdFile = new EX.LibPsd(psdBytes);
+    psdFile.parse();
+    return psdFile;
+  },
+
+
+};
+
+
+module.exports = EX;
---
+++ index.node.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+const fsPr = require('fs/promises');
+
+const LibPsd = require('psd');
+const fflate = require('fflate');
+
+const EX = require('./api.js');
+
+Object.assign(EX, {
+
+  LibPsd,
+
+  async ungzip(data) {
+    return fflate.gunzipSync(data);
+    //  I'm waiting for clarification on how to use the async mode:
+    //  https://github.com/101arrowz/fflate/issues/79 */
+  },
+
+  async fromFile(path) {
+    const data = Uint8Array.from(await fsPr.readFile(path));
+    return EX.fromUint8Array(data);
+  },
+
+
+});
+
+
+module.exports = EX;
